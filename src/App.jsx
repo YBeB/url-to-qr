44dadec473b91ac830234b27ba806a4f8aa78816
ybeb <younes.bouchi@gmail.com>
2025-02-05 15:37:01 +0100

    maj

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,59 @@
-import { useState } from 'react';
+import { useState, useRef } from "react";
 
-import { QRCodeCanvas } from 'qrcode.react'
-
-import { Input , Box ,Card , CardContent,Typography} from '@mui/material';
+import { QRCodeCanvas } from "qrcode.react";
+import "./style.css";
+import { Input, Box, Card, CardContent, Button } from "@mui/material";
 
 function App() {
-  const  [Url ,setUrl]=useState("")
+  const [Url, setUrl] = useState("");
+  const [qrValue, setQrValue] = useState("");
+  const downloadRef = useRef(null);
 
+  function downloadQR() {
+    const qrRefContent = downloadRef.current;
+    if(!qrValue || !qrRefContent ){
+
+alert("Veuillez entrer une URL");
+return
+
+    }else{
+    const imgUrl = qrRefContent.toDataURL("image/png");
+    let temporaryLink = document.createElement("a");
+
+
+    temporaryLink.href = imgUrl;
+    temporaryLink.download = "qrcode.png";
+    document.body.appendChild(temporaryLink);
+    temporaryLink.click();
+    temporaryLink.remove();
+  }
+  
+  }
   return (
-    <>
-
-<Box display="flex"
-  justifyContent="center" 
-  alignItems="center"
-  height="100vh" 
-
->
- <Card sx={{ maxWidth: 300, p: 2, boxShadow: 3 }}>
- <CardContent>
-
-
-  <QRCodeCanvas value={Url}/>
-  <Input
-        type="text"
-        placeholder="Entrez votre url"
-        value={Url}
-        onChange={(e) => setUrl(e.target.value)} 
-      />
-
-  </CardContent>
-  </Card>
-  </Box>
-    </>
-  )
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      height="100vh"
+    >
+      <Card sx={{ maxWidth: 300, p: 2, boxShadow: 3 }}>
+        <CardContent>
+          <div className="Qrcode">
+            <QRCodeCanvas value={qrValue} ref={downloadRef} />
+          </div>
+          <Input
+            type="text"
+            placeholder="Entrez votre url"
+            value={Url}
+            onChange={(e) => setUrl(e.target.value)}
+            sx={{ my: 2, width: "100%" }}
+          />
+          <Button onClick={() => setQrValue(Url)}>Generate</Button>
+          <Button onClick={() => downloadQR()}>Download</Button>
+        </CardContent>
+      </Card>
+    </Box>
+  );
 }
 
-export default App
+export default App;
